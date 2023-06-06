49f3b6a9a1819362ffdcbdfd8accfe5f079fa1ab
JOAN ROY CARLOS VASQUEZ MUNI SJL <joanroycar@gmail.com>
2023-06-06 16:55:44 -0500

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <h1>PERU CAMPEAON</h1>
+    <button v-on:click="disminuir()">-</button>
+    {{ contador }}
+    <button v-on:click="aumentar()">+</button>
+    
+</template>
+
+
+<script>
+
+
+export default {
+
+    data(){
+        return {
+            contador:0
+        }
+    },
+    methods:{
+        disminuir(){
+
+            this.contador--
+            
+        },
+        aumentar(){
+            this.contador++
+
+
+        }
+    }
+    // components:{
+    //     Counter
+    // }
+    
+}
+
+
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
